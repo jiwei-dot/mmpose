ada6492c1e9e36e6a4823e249086597303b0912e
jiwei-dot <weiji@zju.edu.cn>
2023-04-28 10:21:49 +0800

    TGSA-6155 [fix] Fix bug

--- custom_demo/foot_ik.py
+++ custom_demo/foot_ik.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 import numpy as np
 import copy
+import os
 import pickle
 from scipy.spatial.transform import Rotation as R
 
@@ -115,7 +116,11 @@ from utils import load_bvh
 
 
 def save_bvh(args, root_positions_list, rotations_list, seq='ZYX'):
-    with open(args.output, 'w') as fout:
+    
+    new_filename = 'new_' + os.path.basename(args.bvh_file)
+    output_path = os.path.join(args.out_root, new_filename)
+    
+    with open(output_path, 'w') as fout:
         with open(args.bvh_file, 'r') as fin:
             for line in fin:
                 s_line = line.strip()
@@ -168,17 +173,12 @@ def cal_theta_by_length(lc, la, lb):
     return theta
 
 
-def two_bone_ik(joint_offsets, joint_positions, joint_orientations, target_pos):
-    
-    joint_rotations = []
-    for i in range(3):
-        if i == 0:
-            joint_rotations.append(joint_orientations[i])
-        else:
-            joint_rotations.append((R.inv(R.from_quat(joint_orientations[i-1])) * R.from_quat(joint_orientations[i])).as_quat())
+def two_bone_ik(joint_offsets, joint_positions, joint_rotations, joint_orientations, target_pos):
     
     assert len(joint_offsets) == len(joint_positions) == len(joint_orientations) == 3
+    
     new_joint_positions = copy.deepcopy(joint_positions)
+    new_joint_rotations = copy.deepcopy(joint_rotations)
     new_joint_orientations = copy.deepcopy(joint_orientations)
     
     a, b, c = joint_positions
@@ -188,7 +188,8 @@ def two_bone_ik(joint_offsets, joint_positions, joint_orientations, target_pos):
     len_bc = np.linalg.norm(c - b)
     len_at = np.linalg.norm(t - a)
     
-    if (len_at > len_ab + len_bc) or (len_at < abs(len_ab - len_bc)):
+    # step 0: judge 
+    if (len_ab + len_bc) < len_at or (len_ab + len_at) < len_bc or (len_bc + len_at) < len_ab: 
         print(f'len_ab:{len_ab}, leb_bc:{len_bc}, len_at:{len_at}')
         # raise ValueError("Couldn't close to target")
         print("Couldn't close to target")
@@ -228,34 +229,64 @@ def two_bone_ik(joint_offsets, joint_positions, joint_orientations, target_pos):
     return new_joint_positions, new_joint_rotations, new_joint_orientations
     
     
-def foot_ik(offsets, frame_joint_positions, frame_joint_orientations, indices):
+def foot_ik(frame_id, offsets, frame_joint_positions, frame_joint_rotations, frame_joint_orientations, indices, foot_type):
+    
+    foot_height = cal_foot_height(frame_joint_positions, foot_type)
     
     # 1. get joint_offsets, joint_positions, joint_orientations, target_pos
     joint_offsets = offsets[indices]
     joint_positions = frame_joint_positions[indices]
+    joint_rotations = frame_joint_rotations[indices]
     joint_orientations = frame_joint_orientations[indices]
-    target_pos = cal_target_pos(joint_positions[-1])
+    target_pos = cal_target_pos(joint_positions[-1], foot_height)
     
     # 2. two bone ik
-    new_joint_positions, new_joint_rotations, new_joint_orientations = two_bone_ik(joint_offsets, joint_positions, joint_orientations, target_pos)
-    # print(joint_positions[1:])
-    # print(new_joint_positions[1:])
+    new_joint_positions, new_joint_rotations, new_joint_orientations = \
+        two_bone_ik(joint_offsets, joint_positions, joint_rotations, joint_orientations, target_pos)
+
     # 3. foot rotation, rotate foot such that big(small) toe and heel are contact ground 
     # todo
     return new_joint_positions, new_joint_rotations, new_joint_orientations
 
 
-def cal_target_pos(end_effector, height=77/1000.0):
+def cal_target_pos(end_effector, height):
     target_pos = np.array(end_effector)
-    target_pos[1] = height
+    target_pos[-1] = height
     return target_pos
+
+
+def cal_foot_height(frame_joint_positions, foot_type):
+    assert foot_type in ('left', 'right')
+    if foot_type == 'left':
+        point_indices = 3
+        plane_indices = [4, 5, 6]
+    else:
+        point_indices = 9
+        plane_indices = [10, 11, 12]
+    
+    frame_joint_positions = frame_joint_positions[..., :3]
+    vec1 = frame_joint_positions[plane_indices[2]] - frame_joint_positions[plane_indices[0]]
+    vec2 = frame_joint_positions[plane_indices[2]] - frame_joint_positions[plane_indices[1]]
+    norm_vec1 = np.linalg.norm(vec1)
+    norm_vec2 = np.linalg.norm(vec2)
+    vec1 = vec1 / (norm_vec1 + 1e-5)
+    vec2 = vec2 / (norm_vec2 + 1e-5)
+    vec3 = np.cross(vec1, vec2)
+    # plane: Ax + By + Cz + D = 0
+    # (A, B, C) = vec3
+    D = -np.dot(frame_joint_positions[plane_indices[0]], vec3)
+    # print(vec3, D)
+    # print(np.dot(vec3, frame_joint_positions[plane_indices[1]]) + D)
+    # print(np.dot(vec3, frame_joint_positions[plane_indices[2]]) + D)
+    dist = np.abs(np.dot(frame_joint_positions[point_indices], vec3) + D) / (np.linalg.norm(vec3) + 1e-5)
+    return dist
 
 
 def get_parser():
     parser = ArgumentParser()
     
     parser.add_argument('--bvh-file', required=True)
-    parser.add_argument('--output', required=True, default='new.bvh')
+    parser.add_argument('--out-root', required=True)
     parser.add_argument('--footcontact-pkl-file', required=True)
     parser.add_argument('--track-id', default=0, type=int)
     
@@ -263,25 +294,22 @@ def get_parser():
 
 
 def main(args):
-    # use bvh file and foot-contact file
-    names, parents, offsets, video_joint_positions, video_joint_rotations, video_joint_orientations = load_bvh(args.bvh_file)
+
+    # video_joint_rotations:    相对旋转
+    # video_joint_orientations: 绝对旋转
+    names, parents, offsets, video_joint_positions, \
+        video_joint_rotations, video_joint_orientations = load_bvh(args.bvh_file)
     
-    # print(video_joint_positions[0])
-    # with open("workspace/video_h36m_kpts3d.pkl", "rb") as fin:
-    #     data = pickle.load(fin)
-    # print(data[0][0]['keypoints_3d'])  
-    # exit()
+    ground_height = np.min(video_joint_positions[:, :, -1])
+    video_joint_positions[:, :, -1] -= ground_height
     
-    ground_height = np.min(video_joint_positions[:, :, 1])
-    video_joint_positions[:, :, 1] -= ground_height
+    index_Left_hip = names.index('left_hip')
+    index_Left_knee = names.index('left_knee')
+    index_Left_foot = names.index('left_foot')
     
-    index_Left_hip = names.index('Left_hip')
-    index_Left_knee = names.index('Left_knee')
-    index_Left_foot = names.index('Left_foot')
-    
-    index_Right_hip = names.index('Right_hip')
-    index_Right_knee = names.index('Right_knee')
-    index_Right_foot = names.index('Right_foot')
+    index_Right_hip = names.index('right_hip')
+    index_Right_knee = names.index('right_knee')
+    index_Right_foot = names.index('right_foot')
     
     left_indices = [index_Left_hip, index_Left_knee, index_Left_foot]
     right_indices = [index_Right_hip, index_Right_knee, index_Right_foot]
@@ -302,15 +330,19 @@ def main(args):
         frame_joint_orientations = video_joint_orientations[frame_id]
         
         if res[0] and res[1]:
+            # print(video_joint_positions[frame_id][[4, 5, 6]])
             # 左脚触地
-            new_joint_positions, new_joint_rotations, new_joint_orientations = foot_ik(offsets, frame_joint_positions, frame_joint_orientations, left_indices)
+            new_joint_positions, new_joint_rotations, new_joint_orientations = \
+                foot_ik(frame_id, offsets, frame_joint_positions, frame_joint_rotations, frame_joint_orientations, left_indices, 'left')
             frame_joint_positions[left_indices] = new_joint_positions
             frame_joint_rotations[left_indices] = new_joint_rotations
             # video_joint_orientations[left_indices] = new_joint_orientations
             
         if res[2] and res[3]:
+            # print(video_joint_positions[frame_id][[10, 11, 12]])
             # 右脚触地
-            new_joint_positions, new_joint_rotations, new_joint_orientations = foot_ik(offsets, frame_joint_positions, frame_joint_orientations, right_indices)
+            new_joint_positions, new_joint_rotations, new_joint_orientations = \
+                foot_ik(frame_id, offsets, frame_joint_positions, frame_joint_rotations, frame_joint_orientations, right_indices, 'right')
             frame_joint_positions[right_indices] = new_joint_positions
             frame_joint_rotations[right_indices] = new_joint_rotations
             # video_joint_orientations[right_indices] = new_joint_orientations
